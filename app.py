e3cc4cdc7ddac750c4ce459f55ec8c5e58a859e8
Gracious <gpoa@bu.edu>
2025-10-09 20:00:58 -0400

    render deployment changes

--- app.py
+++ app.py
@@ -204,4 +204,5 @@ def logout():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(debug=False, host='0.0.0.0', port=port)
